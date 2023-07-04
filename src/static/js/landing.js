e70fed372c153c0227cdd3e754e5340b01945437
Nijiiro <josephnaval47@gmail.com>
2023-07-04 19:27:37 +0800

    on scroll animation done!

--- src/static/js/landing.js
+++ src/static/js/landing.js
@@ -1,19 +1,29 @@
-const observer = new IntersectionObserver(entries =>{
+const observer = new IntersectionObserver((entries, observer) =>{
     entries.forEach(entry =>{
         if(entry.isIntersecting){
-            document.querySelectorAll('.on-left')[0].classList.add('fade-left')
-            document.querySelectorAll('.on-left')[1].classList.add('fade-left')
-            document.querySelectorAll('.on-left')[2].classList.add('fade-left')
-            document.querySelectorAll('.on-right')[0].classList.add('fade-right')
-            document.querySelectorAll('.on-bottom')[0].classList.add('fade-bottom')
+            if(entry.target.classList.contains('left'))
+            {
+                entry.target.classList.add('fade-left')
+            }
+            if(entry.target.classList.contains('right'))
+            {
+                entry.target.classList.add('fade-right')
+            }
+            if(entry.target.classList.contains('bottom'))
+            {
+                entry.target.classList.add('fade-bottom')
+            }
+
         }else{
-            document.querySelectorAll('.on-left')[0].classList.remove('fade-left')
-            document.querySelectorAll('.on-left')[1].classList.remove('fade-left')
-            document.querySelectorAll('.on-left')[2].classList.remove('fade-left')
-            document.querySelectorAll('.on-right')[0].classList.remove('fade-right')
-            document.querySelectorAll('.on-bottom')[0].classList.remove('fade-bottom')
+            entry.target.classList.remove('fade-left')
+            entry.target.classList.remove('fade-right')
+            entry.target.classList.remove('fade-bottom')
         }
     })
 })
 
-observer.observe(document.querySelector('.about-section'))
+const on_scroll_animation = document.querySelectorAll('.animated')
+on_scroll_animation.forEach(element => observer.observe(element))
+
+
+
