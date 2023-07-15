08e8a249ce9fc0e53a520124294443216189efe7
Nijiiro <josephnaval47@gmail.com>
2023-07-15 16:32:27 +0800

    authentication forms

--- src/scripts/flask/views.py
+++ src/scripts/flask/views.py
@@ -1,17 +1,57 @@
 from flask import Blueprint, redirect, render_template, url_for, request, flash, session
 from src.scripts.backend_func.db_init import mongoDb
 from src.scripts.backend_func.smtp import emailService
+from werkzeug.security import check_password_hash
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['POST','GET'])
 def landing():
-    return render_template('landing.html')
+    if request.method == 'POST':
+
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        session_user =  mongoDb.machica_users.find_one({'email': email},{'_id':0})
+
+        if session_user:
+            if check_password_hash(session_user['password'], password):
+                return render_template('landing.html', user_in_session={'fullname':session_user['fullname'], 'email':session_user['email']})
+            else:
+                flash('Password Incorrrect!', 'danger')
+        else:
+             flash('Email Not Existing', 'danger')
+    
+    return render_template('landing.html', user_in_session=None)
 
 
 @views.route('/services')
 def services():
     return render_template('services.html')
+
+@views.route('/login')
+def login():
+    return render_template('user_login.html')
+
+
+@views.route('/form_validation', methods=['POST'])
+def form_validation():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        #register validations
+        if 'otp' not in data:
+            if data['password'] != data['r_password']:
+                flash('Password not matched', 'warning')
+                return {'status': 401}
+
+            elif mongoDb.machica_users.find_one({'email':data['email']}):
+                flash('Email is already taken!', 'danger')
+                return {'status': 401}
+            
+            elif emailService.send_otp(data['email']):
+                return {'status': 200}
+
 
 
 @views.route('/register', methods=['POST','GET'])
@@ -19,25 +59,12 @@ def register():
     if request.method == 'POST':
         data = request.get_json()
 
-        #register validations
-        if data['password'] != data['r_password']:
-            flash('Password not matched', 'warning')
-            return {'status': 400}
-
-        elif mongoDb.machica_users.find_one({'email':data['email']}):
-            flash('Email is already taken!', 'danger')
-            return {'status': 400}
-        
-        else:
-            if emailService.send_otp(data['email']):
+        if session.get('gen_otp') == data['otp']:
+            if mongoDb.register(data['fullname'],data['email'],data['password']):
                 return {'status': 200}
-
-        if 'otp' in data:
-            if session.get('gen_otp') == data['otp']:
-                if mongoDb.register(data['fullname'],data['email'],data['password']):
-                    flash('Registration successful!', 'success')
-                    # return redirect(url_for('views.landing'))
             else:
-                flash('OTP is not correct', 'danger')
+                flash('Account not registered', 'danger')
+                return {'status': 401}
             
+         
     return render_template('user_register.html')
--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,4 +1,19 @@
 
+
+
+window.onload = () =>{
+  user_data = JSON.parse(sessionStorage.getItem('user_data'))
+  
+  if (user_data != null){
+    document.getElementById('guest-mode').style.display = 'none'
+    document.getElementById('online-mode').style.display = 'block'
+    document.getElementById('logout-btn').innerText = user_data.fullname.split(' ')[0]
+  }
+
+}
+
+
+
 const navbar = document.querySelector('nav')
 
 window.onscroll = function() {
@@ -30,10 +45,18 @@ $("#chatbot-btn").click(()=>{
 
 const user_information = {};
 const generate_otp = document.getElementById("generate-otp");
+const resend_otp = document.getElementById("resend-otp");
+const verify_otp = document.getElementById("verify-otp");
+const otp = document.getElementById("otp");
 const loading_register = document.getElementById("register-spinner");
 const registration_form = document.getElementsByClassName("user-information");
 
-loading_register.style.display = "none";
+const EndpointRequest = async (url,payload) =>{
+    let response = await fetch(url, payload)
+
+    return response.json();
+}
+
 
 generate_otp.onclick = () =>{
     user_information.fullname = registration_form[0].value;
@@ -41,11 +64,10 @@ generate_otp.onclick = () =>{
     user_information.password = registration_form[2].value;
     user_information.r_password = registration_form[3].value;
     
-    loading_register.style.display = "inline";
-    generate_otp.innerText = 'Generating OTP';
-    
 
-    const payload = 
+    generate_otp.innerText = 'Generating OTP...';
+  
+    let payload = 
     {
       method: "POST",
       headers:
@@ -53,14 +75,75 @@ generate_otp.onclick = () =>{
           "Content-Type": "application/json"
       },
       body: JSON.stringify(user_information)
-    }
+    };
 
-  fetch(window.location.href, payload)
-      .then(res => res.json())
-      .then(json => 
-          alert(JSON.stringify(json))
+    EndpointRequest(`${window.origin}/form_validation`,payload)
+      .then(data => {
+          if (data.status != 401){
+            document.getElementById('target-email').innerText = user_information.email;
+            generate_otp.innerText = 'Register';
+            $('#otpModal').modal('show');
+          }else{
+            window.location.reload()
+          };
+        }
       )
+}
 
 
-    // $('#otpModal').modal('show');
+resend_otp.onclick = () =>{
+  resend_otp.innerText = 'Sending new OTP...'
+
+  let payload = 
+  {
+    method: "POST",
+    headers:
+    {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user_information)
+  };
+
+  EndpointRequest(`${window.origin}/form_validation`,payload)
+  .then(data => {
+      if (data.status != 401){
+        document.getElementById('target-email').innerText = user_information.email;
+        resend_otp.innerText = 'Resend';
+      }
+    }
+  )
+}
+
+
+verify_otp.onclick = () =>{
+  verify_otp.innerText = 'Verifying OTP...';
+
+  user_information.otp = otp.value
+  let payload = 
+  {
+    method: "POST",
+    headers:
+    {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user_information)
+  }
+
+  EndpointRequest(window.location.href,payload)
+  .then(data => {
+      if (data.status != 401){
+          alert('Account Registered Successfully');
+          window.location = window.origin
+      }else{
+        window.location.reload()
+      };
+    }
+  )
+}
+
+document.getElementById('logout').onclick = () =>{
+  console.log('bingbong')
+  sessionStorage.clear();
+  window.location = window.location.href;
+  
 }
